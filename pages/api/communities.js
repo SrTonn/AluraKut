2c3f326795b9db2075e1637900933ff21208bc34
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-07-23 08:59:24 -0300

    set modelId as environment variable

--- pages/api/communities.js
+++ pages/api/communities.js
@@ -3,13 +3,11 @@ import { SiteClient } from 'datocms-client'
 export default async function communitiesApi(req, res) {
   if (req.method === 'POST') {
     const TOKEN = process.env.NEXT_PUBLIC_API_DATOCMS_TOKEN_FULL
+    const modelId = process.env.NEXT_PUBLIC_API_DATOCMS_MODEL_ID
     const client = new SiteClient(TOKEN)
     const record = await client.items.create({
-      itemType: '975967', // Model ID for communities
+      itemType: modelId, // Model ID for communities
       ...req.body,
-      // title: 'comunidade test',
-      // imageUrl: 'https://github.com/srtonn.png',
-      // creatorSlug: 'SrTonn',
     })
 
     res.json({
